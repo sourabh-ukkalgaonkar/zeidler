79fe5966edb3f8ed47dd602c5b27b821e80c3550
Sourabh Ukkalgaonkar <sourabh.ukkalgaonkar@gmail.com>
2020-02-17 12:56:08 +0530

    Added feature to sign-in user

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,29 @@
+class SessionsController < ApplicationController
+  before_action :check_active_session, only: [:new, :create]
+
+  def new
+  end
+
+  def create
+    @user = User.find_by(email: params[:email])
+
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect_to root_path
+    else
+      @error = true
+      render :new
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_path
+  end
+
+  private
+
+  def check_active_session
+    redirect_to root_path if current_user.present?
+  end
+end
